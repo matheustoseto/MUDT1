9d6e9dc2e38c253a4e05540a67825a95d788ff12
Unknown <matheus.toseto@hotmail.com>
2017-09-15 22:38:33 -0300

    f

---
+++ Assets/_Project/Scripts/GamePlay/Command.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Command : MonoBehaviour {
+
+    [SerializeField] private ChatMudUI mudUI;
+    [SerializeField] private string namePlayer = "Player";
+
+    private Repositorio repositorio;
+
+	// Use this for initialization
+	void Start () {
+        repositorio = new Repositorio();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GetCommand()
+    {
+        mudUI.LogText += "["+namePlayer+"]: " + falarChat(mudUI.Command.text) + "\n";
+        mudUI.Command.text = "";
+    }
+
+    public string falarChat(string texto)
+    {
+        if ("Examinar".Contains(texto))
+        { // Examinar [sala/objeto]
+
+        }
+        else
+        if ("Mover".Contains(texto))
+        { // Mover [N/S/L/O]
+
+        }
+        else
+        if ("Pegar".Contains(texto))
+        { // Pegar [objeto]
+
+        }
+        else
+        if ("Largar".Contains(texto))
+        { // Largar [objeto]
+
+        }
+        else
+        if ("Inventario".Contains(texto))
+        { // Inventário
+
+        }
+        else
+        if ("Usar".Contains(texto))
+        { // Usar [objeto] {alvo}
+            string[] splitTexto = texto.Split(null);
+            Objeto objeto   = buscarObjeto(splitTexto[1]);
+            Objeto alvo     = buscarObjeto(splitTexto[2]);
+            if (objeto != null && alvo != null)
+            {
+
+            }
+        }
+        else
+        if ("Falar".Contains(texto))
+        { // Falar [texto]
+
+        }
+        else
+        if ("Cochichar".Contains(texto))
+        { // Cochichar [texto] [jogador]
+
+        }
+        else
+        if ("Ajuda".Contains(texto))
+        { // Ajuda
+            return "Lista de Comandos: \n"
+                    + "Examinar [sala/objeto] \n"
+                    + "Mover [N/S/L/O] \n"
+                    + "Pegar [objeto] \n"
+                    + "Largar [objeto] \n"
+                    + "Inventário [sala/objeto] \n"
+                    + "Usar [objeto] {alvo} \n"
+                    + "Falar [texto] \n"
+                    + "Cochichar [texto] [jogador] \n";
+        }
+        return "Não Foi Possível Realizar Essa Ação.";
+    }
+
+    public void acessouJogo(string nome)
+    {
+        Player player = new Player();
+        player.idPlayer = Guid.NewGuid().ToString("N");
+        player.nome = nome;
+
+        repositorio.players.Add(player);
+    }
+
+    public string usarObjeto(TipoObjeto objeto, TipoObjeto alvo)
+    {
+
+
+        return null;
+    }
+
+    public Objeto buscarObjeto(string objeto)
+    {
+        foreach (Objeto obj in repositorio.objetos)
+        {
+            if (obj.nome.Contains(objeto))
+                return obj;
+        }
+        return null;
+    }
+}
---
+++ Assets/_Project/Scripts/GamePlay/Repositorio.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TipoObjeto
+{
+    Machado,
+    Carta,
+    Moeda,
+    Vara_De_Pescar,
+    Tocha,
+    Bota,
+    Espada,
+    Mapa,
+    Lenha,
+    Peixe,
+    Peixe_Assado,
+    Arvores,
+    Rocha,
+    Lago,
+    Ponte_Para_Pesca,
+    Fogueira
+}
+
+public enum IdSalas
+{
+    Sala1,
+    Sala2,
+    Sala3,
+    Sala4,
+    Sala5
+}
+
+public enum Coordenadas
+{
+    Norte,
+    Sul,
+    Leste,
+    Oeste
+}
+
+[System.Serializable]
+public class SalasLigadas
+{
+    public IdSalas sala;
+    public Coordenadas coordenada;
+}
+
+//------ OBJETOS DO JOGO --------
+[System.Serializable]
+public class Objeto
+{
+    public string nome;
+    public TipoObjeto tipo;
+    public bool pegar;
+    public bool usar;
+    public string descricao;
+}
+//------------------------------
+
+//------ PLAYER ----------------
+[System.Serializable]
+public class Player
+{
+    public string idPlayer;
+    public string nome;
+}
+//------------------------------
+
+//------ SALA ------------------
+[System.Serializable]
+public class Sala
+{
+    public string nome;
+    public string descricao;
+    public IdSalas idSala;
+    public List<TipoObjeto> objetos;
+    public List<SalasLigadas> salasLigadas;
+}
+//------------------------------
+
+public class Repositorio : MonoBehaviour
+{
+    public List<Objeto> objetos;
+    public List<Player> players;
+    public List<Sala> salas;
+
+    public Dictionary<Sala, List<Player>> salaComPlayers;
+
+    // Use this for initialization
+    protected void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+
+    }
+}
